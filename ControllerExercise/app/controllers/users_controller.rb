1e08f884a7bb8c7e09d5cdcb9d29d14ba6d736ca
Ben Calabrese <bencalabrese@gmail.com>
2016-03-28 13:43:40 -0700

    developed create and index methods

--- ControllerExercise/app/controllers/users_controller.rb
+++ ControllerExercise/app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def index
+    render json: user_params
+  end
+
+  def create
+    render json: user_params
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :height)
   end
 end
